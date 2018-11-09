b6369bd4de36369550e9548f83ab79bc4b48f457
Tanya <t.lorents2014@yandex.ru>
2018-11-09 09:54:15 +0700

    3 commit

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 """Case-study #2 My first code "Progressive taxation"
 Developers:
 Kartashova A.,Lorents T.
+
+# string constants
+name_month = [JAN, FAB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
+
+annual_income = 0
+for month in range(12):
+    print('{} {}:'.format(QUESTION,name_month[month], end ='')
+    income = float(input())
+    annual_income += income
+print(annual_income)

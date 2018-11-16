23c7d52a2ff261b9cb09938a0e68319f0bc4e119
Ann <annagirl@ngs.ru>
2018-11-16 08:39:03 +0700

    4 commit

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ Kartashova A.,Lorents T.
 
 # string constants
 name_month = [JAN, FAB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
-
+familystatus =
 annual_income = 0
 for month in range(12):
     print('{} {}:'.format(QUESTION,name_month[month], end ='')
     income = float(input())
     annual_income += income
 print(annual_income)
+D1 = 0
+D2 = 9076
+D3 = 36901
+D4 = 89351
+D5 = 186351
+D6 = 405101
+D7 = 406751
+
+

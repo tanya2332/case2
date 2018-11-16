c905988dbf8c1bbc42b6c2e960ce57836dd161b6
Ann <annagirl@ngs.ru>
2018-11-16 09:13:40 +0700

    8 commit

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ Kartashova A.,Lorents T.
 
 # string constants
 name_month = [JAN, FAB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
-familystatus =
 annual_income = 0
 for month in range(12):
     print('{} {}:'.format(QUESTION,name_month[month], end ='')
@@ -15,8 +14,8 @@ subj_1 = "Если вы один субъект, то введите 1,"
 subj_2 = "если вы супружеская пара, то введите 2,"
 subj_3 = "если вы родитель-одиночка, то введите 3:"
 print(subj_1,subj_2,subj_3)
-vvod = int(input())
-if  vvod == 1:
+familystatus = int(input())
+if  familystatus == 1:
     D1 = 0
     D2 = 9076
     D3 = 36901
@@ -24,7 +23,29 @@ if  vvod == 1:
     D5 = 186351
     D6 = 405101
     D7 = 406751
-elif vvod == 2:
+    if annual_income < D2:
+        Sn = 10
+        N = Sn * (annual_income - D1)
+    elif D2 <= annual_income < D3:
+        Sn = 15
+        N = Sn * (annual_income - D2)
+    elif D3 <= annual_income < D4:
+        Sn = 25
+        N = Sn * (annual_income - D3)
+    elif D3 <= annual_income < D4:
+        Sn = 28
+        N = Sn * (annual_income - D4)
+    elif D4 <= annual_income < D5:
+        Sn = 33
+        N = Sn * (annual_income - D5)
+    elif D5 <= annual_income < D6:
+        Sn = 35
+        N = Sn * (annual_income - D6)
+    elif D6 <= annual_income < D7:
+        Sn = 39.6
+        N = Sn * (annual_income - D7)
+
+elif familystatus == 2:
     D1 = 0
     D2 = 18151
     D3 = 73801
@@ -32,6 +53,28 @@ elif vvod == 2:
     D5 = 226851
     D6 = 405101
     D7 = 457601
+    if annual_income < D2:
+        Sn = 10
+        N = Sn * (annual_income - D1)
+    elif D2 <= annual_income < D3:
+        Sn = 15
+        N = Sn * (annual_income - D2)
+    elif D3 <= annual_income < D4:
+        Sn = 25
+        N = Sn * (annual_income - D3)
+    elif D3 <= annual_income < D4:
+        Sn = 28
+        N = Sn * (annual_income - D4)
+    elif D4 <= annual_income < D5:
+        Sn = 33
+        N = Sn * (annual_income - D5)
+    elif D5 <= annual_income < D6:
+        Sn = 35
+        N = Sn * (annual_income - D6)
+    elif D6 <= annual_income < D7:
+        Sn = 39.6
+        N = Sn * (annual_income - D7)
+
 else:
     D1 = 0
     D2 = 12951
@@ -40,6 +83,27 @@ else:
     D5 = 206601
     D6 = 405101
     D7 = 432201
-if vvod =
+    if annual_income < D2:
+        Sn = 10
+        N = Sn * (annual_income - D1)
+    elif D2 <= annual_income < D3:
+        Sn = 15
+        N = Sn * (annual_income - D2)
+    elif D3 <= annual_income < D4:
+        Sn = 25
+        N = Sn * (annual_income - D3)
+    elif D3 <= annual_income < D4:
+        Sn = 28
+        N = Sn * (annual_income - D4)
+    elif D4 <= annual_income < D5:
+        Sn = 33
+        N = Sn * (annual_income - D5)
+    elif D5 <= annual_income < D6:
+        Sn = 35
+        N = Sn * (annual_income - D6)
+    elif D6 <= annual_income < D7:
+        Sn = 39.6
+        N = Sn * (annual_income - D7)
 
+print(N)
 

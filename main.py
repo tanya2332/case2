759520a14329f26a2a6e6cda4c47173a73a06a72
Dan4411 <44485889+Dan4411@users.noreply.github.com>
2018-11-16 18:00:41 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 """Case-study #2 My first code "Progressive taxation"
 Developers:
-Kartashova A.,Lorents T., Taranetz D.
-
+Kartashova A.,Lorents T., Taranets D.
 """
-import local.py
-# string constants
-name_month = [JAN, FAB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
-annual_income = 0
-for month in range(12):
-    print('{} {}:'.format(QUESTION,name_month[month], end ='')
-    income = float(input())
-    annual_income += income
-print(annual_income)
+import local
+# Ввод доходов по месяцам
+jan = int(input(local.january))
+feb = int(input(local.february))
+mar = int(input(local.march))
+apr = int(input(local.april))
+may = int(input(local.may))
+jun = int(input(local.june))
+jul = int(input(local.july))
+aug = int(input(local.august))
+sep = int(input(local.september))
+okt = int(input(local.october))
+nov = int(input(local.november))
+dec = int(input(local.december))
+annual_income = jan + feb + mar + apr + may + jun + jul + aug + sep + okt + nov + dec
+
+# Ввод статуса
+
 subj_1 = local.one_subj
 subj_2 = local.married
 subj_3 = local.single_parent
 print(subj_1,subj_2,subj_3)
 familystatus = int(input())
+
+# Процент налогов
+
+Sn1 = 0.10
+Sn2 = 0.15
+Sn3 = 0.25
+Sn4 = 0.28
+Sn5 = 0.33
+Sn6 = 0.35
+Sn7 = 0.396
+
+# Расчет налогов
+
 if  familystatus == 1:
     D1 = 0
     D2 = 9076
@@ -26,26 +47,19 @@ if  familystatus == 1:
     D6 = 405101
     D7 = 406751
     if annual_income < D2:
-        Sn = 10
-        N = Sn * (annual_income - D1)
+        N = Sn1 * (annual_income - D1)
     elif D2 <= annual_income < D3:
-        Sn = 15
-        N = Sn * (annual_income - D2)
+        N = Sn1 * (D2 - D1) + Sn2 * (annual_income - D2)
     elif D3 <= annual_income < D4:
-        Sn = 25
-        N = Sn * (annual_income - D3)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (annual_income - D3)
     elif D3 <= annual_income < D4:
-        Sn = 28
-        N = Sn * (annual_income - D4)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (D4 - D3) + Sn4 * (annual_income - D4)
     elif D4 <= annual_income < D5:
-        Sn = 33
-        N = Sn * (annual_income - D5)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (D4 - D3) + Sn4 * (D5 - D4) + Sn5 * (annual_income - D5)
     elif D5 <= annual_income < D6:
-        Sn = 35
-        N = Sn * (annual_income - D6)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (D4 - D3) + Sn4 * (D5 - D4) + Sn5 * (D6 - D5) + Sn6 * (annual_income - D6)
     elif D6 <= annual_income < D7:
-        Sn = 39.6
-        N = Sn * (annual_income - D7)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (D4 - D3) + Sn4 * (D5 - D4) + Sn5 * (D6 - D5) + Sn6 * (D7 - D6) + Sn7 * (annual_income - D7)
 
 elif familystatus == 2:
     D1 = 0
@@ -56,28 +70,21 @@ elif familystatus == 2:
     D6 = 405101
     D7 = 457601
     if annual_income < D2:
-        Sn = 10
-        N = Sn * (annual_income - D1)
+        N = Sn1 * (annual_income - D1)
     elif D2 <= annual_income < D3:
-        Sn = 15
-        N = Sn * (annual_income - D2)
+        N = Sn1 * (D2 - D1) + Sn2 * (annual_income - D2)
     elif D3 <= annual_income < D4:
-        Sn = 25
-        N = Sn * (annual_income - D3)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (annual_income - D3)
     elif D3 <= annual_income < D4:
-        Sn = 28
-        N = Sn * (annual_income - D4)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (D4 - D3) + Sn4 * (annual_income - D4)
     elif D4 <= annual_income < D5:
-        Sn = 33
-        N = Sn * (annual_income - D5)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (D4 - D3) + Sn4 * (D5 - D4) + Sn5 * (annual_income - D5)
     elif D5 <= annual_income < D6:
-        Sn = 35
-        N = Sn * (annual_income - D6)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (D4 - D3) + Sn4 * (D5 - D4) + Sn5 * (D6 - D5) + Sn6 * (annual_income - D6)
     elif D6 <= annual_income < D7:
-        Sn = 39.6
-        N = Sn * (annual_income - D7)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (D4 - D3) + Sn4 * (D5 - D4) + Sn5 * (D6 - D5) + Sn6 * (D7 - D6) + Sn7 * (annual_income - D7)
 
-else:
+elif familystatus == 3:
     D1 = 0
     D2 = 12951
     D3 = 49401
@@ -86,26 +93,18 @@ else:
     D6 = 405101
     D7 = 432201
     if annual_income < D2:
-        Sn = 10
-        N = Sn * (annual_income - D1)
+        N = Sn1 * (annual_income - D1)
     elif D2 <= annual_income < D3:
-        Sn = 15
-        N = Sn * (annual_income - D2)
+        N = Sn1 * (D2 - D1) + Sn2 * (annual_income - D2)
     elif D3 <= annual_income < D4:
-        Sn = 25
-        N = Sn * (annual_income - D3)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (annual_income - D3)
     elif D3 <= annual_income < D4:
-        Sn = 28
-        N = Sn * (annual_income - D4)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (D4 - D3) + Sn4 * (annual_income - D4)
     elif D4 <= annual_income < D5:
-        Sn = 33
-        N = Sn * (annual_income - D5)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (D4 - D3) + Sn4 * (D5 - D4) + Sn5 * (annual_income - D5)
     elif D5 <= annual_income < D6:
-        Sn = 35
-        N = Sn * (annual_income - D6)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (D4 - D3) + Sn4 * (D5 - D4) + Sn5 * (D6 - D5) + Sn6 * (annual_income - D6)
     elif D6 <= annual_income < D7:
-        Sn = 39.6
-        N = Sn * (annual_income - D7)
+        N = Sn1 * (D2 - D1) + Sn2 * (D3 - D2) + Sn3 * (D4 - D3) + Sn4 * (D5 - D4) + Sn5 * (D6 - D5) + Sn6 * (D7 - D6) + Sn7 * (annual_income - D7)
 
 print(N)
-
